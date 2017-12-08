9f842ad89b0bc8dc86baa93c34a4f85827ee578f
June <june_young@yeah.net>
2017-12-08 20:34:56 +0800

    First commit.

---
+++ app/src/main/java/com/june/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.june.coolweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //https://github.com/yangyi724/coolweather.git
+    }
+}
